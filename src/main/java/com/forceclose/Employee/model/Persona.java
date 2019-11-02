c74e69066fbfe471b53a8f1497ebf7f6ea6e95e3
Anthony Puitiza Lopez <anthony.puitiza.02@gmail.com>
2019-11-02 02:28:00 -0500

    Agregando lombok en el proyecto para simplificar mucho los getter y setter

--- src/main/java/com/forceclose/Employee/model/Persona.java
+++ src/main/java/com/forceclose/Employee/model/Persona.java
@@ -1,40 +1,20 @@
 package com.forceclose.Employee.model;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
+@Data
 @Entity
 @Table(name = "Persona")
 public class Persona {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     @Column
     private String name;
     @Column
     private String lastName;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
