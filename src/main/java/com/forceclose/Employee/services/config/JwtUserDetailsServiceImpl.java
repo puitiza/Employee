8d03c132745bcf72fb75371535e61b67cba28699
Anthony Puitiza -- Centro <anthony.puitiza.02@gmail.com>
2019-11-21 18:14:37 -0500

    cambios necesarios para compilar en la pc de lenovo

--- src/main/java/com/forceclose/Employee/services/config/JwtUserDetailsServiceImpl.java
+++ src/main/java/com/forceclose/Employee/services/config/JwtUserDetailsServiceImpl.java
@@ -1,6 +1,5 @@
 package com.forceclose.Employee.services.config;
 
-import com.forceclose.Employee.config.jwt.JwtAuthorizationFilter;
 import com.forceclose.Employee.config.jwt.JwtTokenUtil;
 import com.forceclose.Employee.config.jwt.JwtUserInfo;
 import com.forceclose.Employee.model.entity.access.UserAccess;
@@ -9,6 +8,7 @@ import com.forceclose.Employee.model.request.UserRequest;
 import com.forceclose.Employee.repository.access.RoleRepository;
 import com.forceclose.Employee.repository.access.UserRepository;
 import com.forceclose.Employee.repository.access.relation.User_RoleRepository;
+import com.forceclose.Employee.services.mail.EmailServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +41,9 @@ public class JwtUserDetailsServiceImpl implements UserDetailsService, JwtUserDet
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    public EmailServiceImpl emailService;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         try {
@@ -57,28 +60,42 @@ public class JwtUserDetailsServiceImpl implements UserDetailsService, JwtUserDet
     @Transactional
     @Override
     public UserAccess register(UserRequest user) {
-        UserAccess newUser = new UserAccess();
-        newUser.setUsername(user.getUsername());
-        newUser.setPassword(passwordEncoder.encode(user.getPassword()));
-        newUser.setActivated(true);
-        newUser.setDateCreated(new Date());
-        newUser.setUserCreated(jwtTokenUtil.getUsernameLogged());
-        UserAccess finalUser = userRepository.save(newUser);
+        try {
+            UserAccess newUser = new UserAccess();
+            newUser.setUsername(user.getUsername());
+            newUser.setPassword(passwordEncoder.encode(user.getPassword()));
+            newUser.setActivated(true);
+            newUser.setDateCreated(new Date());
+            newUser.setUserCreated(jwtTokenUtil.getUsernameLogged());
+            UserAccess finalUser = userRepository.save(newUser);
 
-        final Set<UserRole> userRoles = new HashSet<>();
-        user.getRoles().forEach(p -> {
+            final Set<UserRole> userRoles = new HashSet<>();
+            user.getRoles().forEach(p -> {
 
-            UserRole newUserRole = new UserRole();
-            newUserRole.setUser(finalUser);
-            newUserRole.setRole(roleRepository.findByName(p));
-            newUserRole.setActivated(true);
-            newUserRole.setDateCreated(new Date());
+                UserRole newUserRole = new UserRole();
+                newUserRole.setUser(finalUser);
+                newUserRole.setRole(roleRepository.findByName(p));
+                newUserRole.setActivated(true);
+                newUserRole.setDateCreated(new Date());
 
-            UserRole userRole = user_RoleRepository.save(newUserRole);
-            userRoles.add(userRole);
-        });
-        finalUser.setUserRole(userRoles);
-        return userRepository.save(finalUser);
+                UserRole userRole = user_RoleRepository.save(newUserRole);
+                userRoles.add(userRole);
+            });
+            finalUser.setUserRole(userRoles);
+
+            UserAccess newUserAccess = userRepository.save(finalUser);
+            if(newUserAccess!=null){
+                emailService.sendSimpleMessage("anthony.puitiza.02@gmail.com",
+                        "CREACIÓN DE NUEVO USUARIO: " + newUserAccess.getUsername(),
+                        "Se ha creado con éxito este nuevo usuario perro");
+            }
+
+            return newUserAccess;
+        }catch (Exception e){
+            String ya=e.getMessage();
+            return null;
+        }
+
 
     }
 }
---
+++ src/main/java/com/forceclose/Employee/services/mail/EmailService.java
@@ -0,0 +1,20 @@
+package com.forceclose.Employee.services.mail;
+
+import org.springframework.mail.SimpleMailMessage;
+
+/**
+ * Created by Olga on 8/22/2016.
+ */
+public interface EmailService {
+    void sendSimpleMessage(String to,
+                           String subject,
+                           String text);
+    void sendSimpleMessageUsingTemplate(String to,
+                                        String subject,
+                                        SimpleMailMessage template,
+                                        String... templateArgs);
+    void sendMessageWithAttachment(String to,
+                                   String subject,
+                                   String text,
+                                   String pathToAttachment);
+}
